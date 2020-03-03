fcf42f9963ad89489a95320a39410435c290d30a
Pitipat Chairoj <pitipat1998@gmail.com>
2020-03-03 18:00:28 +0700

    fix parallel map

--- src/primitive/par_map.rs
+++ src/primitive/par_map.rs
@@ -1,23 +1,25 @@
 extern crate rayon;
 use rayon::prelude::*;
+use std::fmt::Debug;
 
 const THRESHOLD: usize = 100;
 
-fn par_map_recurse<T: Sync, U: Sync + Send>(seq: &mut Vec<T>, func: &dyn Fn(usize, &T) -> U, ret: &mut Vec<U>, start: usize, stop: usize) {
+fn par_map_recurse<T: Sync + Send, U: Sync + Send, W: (FnMut(usize, &T) -> U) + Sync + Send>(seq: &mut Vec<T>, func: &mut W, ret: &mut Vec<U>, start: usize, stop: usize) {
     let n = stop-start;
+    println!("start={}, stop={}", start, stop);
     if n <= THRESHOLD {
-        (start..stop).into_par_iter().for_each(|i| {
-            ret[i] = func(i, &seq[i])
-        })
+        for i in start..stop {
+            ret[i] = func(i, &seq[i]);
+        }
     } else {
         let half: usize = (stop - start) / 2;
         par_map_recurse(seq, func, ret,start, half);
-        par_map_recurse(seq,func,ret, half + 1, stop);
+        par_map_recurse(seq, func, ret, half + 1, stop);
     }
 }
 
-pub fn par_map<T: Sync, U: Sync + Send>(seq: &mut Vec<T>, func: &dyn Fn(usize, &T) -> U) -> Vec<U> {
-    let mut ret: Vec<U> = Vec::new();
+pub fn par_map<T: Sync + Send + Copy, U: Sync + Send + Copy + Debug, W: (FnMut(usize, &T) -> U) + Sync + Send>(seq: &mut Vec<T>, func: &mut W) -> Vec<U> {
+    let mut ret: Vec<U> = Vec::with_capacity(seq.len());
     unsafe { ret.set_len(seq.len()) }
     par_map_recurse(seq, func, &mut ret,0, seq.len());
     ret
