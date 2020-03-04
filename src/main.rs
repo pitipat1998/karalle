9c24361c420289fc036eae56b0557d68823ba179
Nuttapat Koonarangsri <webmaster@hackinteach.com>
2020-03-04 19:08:07 +0700

    benchmark par-map v1

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,16 @@
-extern crate csv;
-
 use std::io;
 use std::time::{Duration, Instant};
 
+pub mod util;
+pub mod primitive;
+use crate::util::file_reader::read_csv;
+use crate::primitive::par_map;
+
 fn main() {
-    let mut rdr = csv::Reader::from_reader(io::stdin());
-    let mut v: Vec<i32> = Vec::new();
-
-    for result in rdr.records() {
-        // The iterator yields Result<StringRecord, Error>, so we check the
-        // error here.
-        let record = result.unwrap();
-        let number = record.get(0).unwrap().parse::<i32>().unwrap();
-        v.push(number)
-    }
-    println!("{:?}", v);
+    let v: Vec<i32> = read_csv("../data_medium.csv");
+    let f= |_j: usize, a: &i32| -> i32 { if *a <= 2 { 1 } else { 0 } };
     let now = Instant::now();
-
+    let o = par_map(&v, f);
+    let stop = now.elapsed();
+    println!("Time used: {:?} ns", stop.as_nanos())
 }
