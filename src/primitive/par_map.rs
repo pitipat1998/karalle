f48d558698b8802470dbed8a927be2776da0fbe8
Nuttapat Koonarangsri <webmaster@hackinteach.com>
2020-03-05 14:28:17 +0700

    implement O(1) span

--- src/primitive/par_map.rs
+++ src/primitive/par_map.rs
@@ -46,7 +46,7 @@ where T: Sync + Send,
     }
 }
 
-pub fn par_map<'p, T, U, V>(seq: &Vec<T>, func: V) -> Vec<U>
+pub fn par_map_v1<'p, T, U, V>(seq: &Vec<T>, func: V) -> Vec<U>
     where T: Sync + Send,
           U: Sync + Send,
           V: Sync + Send + (Fn(usize, &T) -> U)
@@ -54,5 +54,30 @@ pub fn par_map<'p, T, U, V>(seq: &Vec<T>, func: V) -> Vec<U>
     let mut ret: Vec<U> = Vec::with_capacity(seq.len());
     unsafe { ret.set_len(seq.len()) }
     par_map_utils(seq, &mut ret, &func, 0, seq.len());
+    ret
+}
+
+fn par_map_util_v2<T, U, V>(seq: &[T], ret: &mut [U], func: &V, s: usize, e: usize)
+    where T: Sync + Send,
+          U: Sync + Send,
+          V: Sync + Send + (Fn(usize, &T) -> U)
+{
+    rayon::scope(|s| {
+        for (i, chunk) in ret.chunks_mut(1).enumerate() {
+            s.spawn(move |_| {
+                chunk[0] = func(i, &seq[i]);
+            });
+        }
+    })
+}
+
+pub fn par_map_v2<'p, T, U, V>(seq: &Vec<T>, func: V) -> Vec<U>
+    where T: Sync + Send,
+          U: Sync + Send,
+          V: Sync + Send + (Fn(usize, &T) -> U)
+{
+    let mut ret: Vec<U> = Vec::with_capacity(seq.len());
+    unsafe { ret.set_len(seq.len()) }
+    par_map_util_v2(seq, &mut ret, &func,0, seq.len());
     ret
 }
