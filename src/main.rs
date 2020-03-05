031c140defb06592732e73bcf71110b4758c1e3f
Nuttapat Koonarangsri <webmaster@hackinteach.com>
2020-03-05 16:00:27 +0700

    read all files in data/

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 use std::collections::HashMap;
-use std::io;
+use std::{fs,io};
 use std::time::{Duration, Instant};
 
 use crate::primitive::*;
 use crate::util::file_reader::read_csv;
+use std::path::{PathBuf, Path};
+use std::fmt::Error;
+use std::process::exit;
 
 pub mod util;
 pub mod primitive;
-
-fn small_compute(i: usize, e: &u128) -> u128 {
-    *e + *e
-}
+//
+// fn small_compute(i: usize, e: &u128) -> u128 {
+//     *e + *e
+// }
 
 fn fac(i: &u128) -> u128 {
     if (*i) <= 1 {
@@ -20,9 +23,9 @@ fn fac(i: &u128) -> u128 {
     }
 }
 
-fn huge_compute(i: usize, e: &u128) -> u128 {
-    fac(e)
-}
+// fn huge_compute(i: usize, e: &u128) -> u128 {
+//     fac(e)
+// }
 
 fn benchmark_v1<V>(file: &str, func: V) -> Duration
     where V: Sync + Send + (Fn(usize, &u128) -> u128)
@@ -43,11 +46,23 @@ fn benchmark_v2<V>(file: &str, func: V) -> Duration
 }
 
 fn main() {
-    // let files = vec!["../data_small.csv"];
-    let files = vec!["../data_small.csv", "../data_medium.csv", "../data_large.csv"];
+    let files: Vec<String> = fs::read_dir("data").unwrap()
+        .into_iter()
+        .map(|res| {
+            res.unwrap().path().into_os_string()
+        })
+        .filter(|e|{
+            e.to_os_string().into_string().unwrap().ends_with(".csv")
+        })
+        .map(|e|e.into_string().unwrap())
+        .collect();
+    if files.is_empty() {
+        println!("No data to be testing on, put .csc files in data/");
+        exit(-1);
+    }
     let mut func: HashMap<&str, &(dyn Sync + Send + Fn(usize, &u128) -> u128)> = HashMap::new();
-    func.insert("Small_C", &small_compute);
-    func.insert("Huge_C", &huge_compute);
+    func.insert("Small_C", &|i, x| { *x * *x });
+    func.insert("Huge_C", &|i, x| { fac(x) });
 
     let mut result: HashMap<String, Duration> = HashMap::new();
     for (&name, &f) in &func {
