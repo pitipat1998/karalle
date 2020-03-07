6a7d8e35ba04a4625e60e1f9638ae73f804a28d5
Nuttapat Koonarangsri <webmaster@hackinteach.com>
2020-03-08 00:07:21 +0700

    finish reading nested list for flatten

--- src/util/file_reader.rs
+++ src/util/file_reader.rs
@@ -4,6 +4,14 @@ extern crate num;
 use num::{PrimInt, Unsigned};
 use std::iter::Product;
 use std::str::FromStr;
+use std::fs::*;
+use std::io;
+use std::path::Path;
+use std::io::BufRead;
+use rayon::str::*;
+use serde_json::from_str;
+use serde::de::DeserializeOwned;
+
 
 #[allow(dead_code)]
 pub fn read_csv<T>(path: &str) -> Vec<T>
@@ -17,6 +25,26 @@ where T: PrimInt + Unsigned + Product + FromStr,
         let record = result.unwrap();
         let number = record.get(0).unwrap().parse::<T>().unwrap();
         v.push(number)
+    }
+    v
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+    where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+#[allow(dead_code)]
+pub fn read_nested<T: DeserializeOwned>(path: &str) -> Vec<Vec<T>> {
+    let mut v: Vec<Vec<T>> = Vec::new();
+    if let Ok(lines) = read_lines(path) {
+        for line_op in lines {
+            if let Ok(line) = line_op {
+                let x: Vec<T> = (line.split(",").map(|i| from_str::<T>(i).unwrap()).collect());
+                v.push(x);
+            }
+        }
     }
     v
 }
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,3 +1,4 @@
 pub mod file_reader;
 
 pub use self::file_reader::read_csv;
+pub use self::file_reader::read_nested;
