ebcd50140256c3e27f8701b065b4c9fae0812c14
Pitipat Chairoj <pitipat1998@gmail.com>
2020-03-12 13:23:55 +0700

    qs benchmark

--- src/benchmark/mod.rs
+++ src/benchmark/mod.rs
@@ -3,4 +3,4 @@ mod flatten;
 mod quick_sort;
 pub use self::map::run_map_benchmark;
 pub use self::flatten::run_flatten_benchmark;
-//pub use self::quick_sort::run_quick_sort_benchmark;
+pub use self::quick_sort::run_quick_sort_benchmark;
--- src/benchmark/quick_sort.rs
+++ src/benchmark/quick_sort.rs
@@ -1,41 +1,68 @@
-//use std::collections::HashMap;
-//use std::time::{Duration, Instant};
-//
-//use crate::primitive::*;
-//use crate::sort::*;
-//
-//// par_flatten<T>(seqs: &Vec<&Vec<T>>) -> Vec<T>
-//#[allow(dead_code)]
-//type QuicksortFunc<T> = dyn Sync + Send + Fn(&Vec<&Vec<T>>) -> Vec<T>;
-//
-//#[allow(dead_code)]
-//fn benchmark_quick_sort<T: Copy + Sync, K>(vec: &Vec<T>, flat: K, rounds: u128) -> Duration
-//    where K: Sync + Send + (Fn(&Vec<Vec<T>>) -> Vec<T>)
-//{
-//    let now = Instant::now();
-//    for _ in 0..rounds {
-//        let _ = flat(&vec);
-//    }
-//    now.elapsed().div_f32(rounds as f32)
-//}
-//
-//#[allow(dead_code)]
-//pub fn run_quick_sort_benchmark<T>(
-//    d: &String,
-//    v: &Vec<T>,
-//    rounds: u128,
-//    threads: usize
-//) -> HashMap<String, Duration>
-//where T: Copy + Sync + Send
-//{
-//    let mut result: HashMap<String, Duration> = HashMap::new();
-//
-//    let key = format!("{}, {}, par_quick_sort", &d, threads);
-//    let duration = benchmark_quick_sort(&v, par_quick_sort, rounds);
-//    result.entry(key).or_insert(duration);
-//
-//    let key = format!("{}, {}, par_quick_sort_v2", &d, threads);
-//    let duration = benchmark_quick_sort(&v, par_quick_sort_v2, rounds);
-//    result.entry(key).or_insert(duration);
-//    result
-//}
+use std::collections::HashMap;
+use std::time::{Duration, Instant};
+
+use crate::primitive::*;
+use crate::sort::*;
+
+#[allow(dead_code)]
+fn benchmark_quick_sort<T, U>(vec: &Vec<T>, func: &U, rounds: u128) -> Duration
+    where T: Copy + Sync + Send,
+          U: Sync + Send + Fn(&T, &T) -> i32
+{
+let now = Instant::now();
+    for _ in 0..rounds {
+        let _ = par_quick_sort(vec, func);
+    }
+    now.elapsed().div_f32(rounds as f32)
+}
+
+#[allow(dead_code)]
+fn benchmark_quick_sort_v2<T, U>(vec: &mut Vec<T>, func: &U, rounds: u128) -> Duration
+    where T: Copy + Sync + Send,
+          U: Sync + Send + Fn(&T, &T) -> i32
+{
+    let now = Instant::now();
+    for _ in 0..rounds {
+        par_quick_sort_v2(vec, func);
+    }
+    now.elapsed().div_f32(rounds as f32)
+}
+
+#[allow(dead_code)]
+fn benchmark_quick_sort_v3<T, U>(vec: &mut Vec<T>, func: &U, rounds: u128) -> Duration
+    where T: Copy + Sync + Send,
+          U: Sync + Send + Fn(&T, &T) -> i32
+{
+    let now = Instant::now();
+    for _ in 0..rounds {
+        par_quick_sort_v3(vec, func);
+    }
+    now.elapsed().div_f32(rounds as f32)
+}
+
+#[allow(dead_code)]
+pub fn run_quick_sort_benchmark<T, U>(
+    d: &String,
+    v: &mut Vec<T>,
+    f: U,
+    rounds: u128,
+    threads: usize
+) -> HashMap<String, Duration>
+where T: Copy + Sync + Send,
+      U: Sync + Send + Fn(&T, &T) -> i32
+{
+let mut result: HashMap<String, Duration> = HashMap::new();
+
+    let key = format!("{}, {}, par_quick_sort", &d, threads);
+    let duration = benchmark_quick_sort(&v, &f , rounds);
+    result.entry(key).or_insert(duration);
+
+    let key = format!("{}, {}, par_quick_sort_v2", &d, threads);
+    let duration = benchmark_quick_sort_v2(v, &f, rounds);
+    result.entry(key).or_insert(duration);
+
+    let key = format!("{}, {}, par_quick_sort_v3", &d, threads);
+    let duration = benchmark_quick_sort_v3(v, &f, rounds);
+    result.entry(key).or_insert(duration);
+    result
+}
