5af9f0b19395249788cf7515e75d3f1a7c59d8e9
Nuttapat Koonarangsri <webmaster@hackinteach.com>
2020-03-30 20:16:33 +0700

    fix make

--- src/main.rs
+++ src/main.rs
@@ -94,8 +94,10 @@ fn main() {
     let sizes: Vec<u64> = vec_init(max_size, &|i| { (1 << (i + 1)) as u64 }, 2000);
     let make_type = envmnt::get_or("KMAKE", "none").to_lowercase();
 
-    if &make_type != "none" { return; }
-    else {make_file(&make_type);}
+    if &make_type != "none" {
+        make_file(&make_type);
+        return;
+    }
 
     let mut tn: usize = envmnt::get_or("KTHREAD", "0").parse().unwrap();
     if tn == 0 {
